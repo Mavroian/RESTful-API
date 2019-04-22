f3ba4ea25f8d423f6025a49ec1b057345210de33
Mavroian and Yasuhito Miyazaki <mavflorin+kizayami.yasu@gmail.com>
2019-04-22 17:45:47 +0900

    Working on RESTful design

--- src/server.js
+++ src/server.js
@@ -1,4 +1,43 @@
-const pokeData = require("./data");
+const { pokemon, attacks, types } = require("./data");
+const express = require("express");
+
+const createServer = () => {
+  const app = express();
+  app.get("/api/pokemons", (req, res) => {
+    const { limit } = req.query;
+    if (limit) {
+      const poke = [];
+      for (let i = 0; i < limit; i++) {
+        poke.push(pokemon[i]);
+      }
+      res.send(poke);
+    } else {
+      res.send(pokemon);
+    }
+  });
+  app.use(express.json());
+
+  app.post("/api/pokemons", (req, res) => {
+    res.json(req.body);
+  });
+
+  app.get("/api/pokemons/:param", (req, res) => {
+    const { param } = req.params;
+    for (const poke of pokemon) {
+      if (~~param > 1) {
+        if (~~poke.id === ~~param) {
+          res.send(poke);
+        }
+      } else if (poke.name.toLowerCase() === param.toLowerCase()) {
+        res.send(poke);
+      }
+    }
+  });
+  app.patch("");
+
+  return app;
+};
+module.exports = { createServer };
 /**
  * Use this file to create and set up your express server
  */
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -1,16 +1,81 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
-const server = require("../src/server");
-
+const { createServer } = require("../src/server");
+chai.should();
+const { pokemon, attacks, types } = require("../src/data/index");
 /*
  * This sprint you will have to create all tests yourself, TDD style.
  * For this you will want to get familiar with chai-http https://www.chaijs.com/plugins/chai-http/
  * The same kind of structure that you encountered in lecture.express will be provided here.
  */
+const server = createServer();
 describe("Pokemon API Server", () => {
   let request;
   beforeEach(() => {
     request = chai.request(server);
+  });
+  describe("GET /api/pokemons", () => {
+    it("should return the full list of Pokemon", async () => {
+      const res = await request.get("/api/pokemons");
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal(pokemon);
+    });
+    it("should b able to take a query parameter 'limit=n'", async () => {
+      const res = await request.get("/api/pokemons/").query({ limit: 5 });
+      const fivePokemons = [];
+      for (let i = 0; i < 5; i++) {
+        fivePokemons.push(pokemon[i]);
+      }
+      res.should.be.json;
+      JSON.parse(res.text).should.eql(fivePokemons);
+    });
+  });
+  describe("POST /api/pokemons", () => {
+    it("should add a Pokemon.", async () => {
+      const poke = pokemon[0];
+      const res = await request.post("/api/pokemons").send(poke);
+
+      JSON.parse(res.text).should.eql(poke);
+    });
+  });
+  describe("GET /api/pokemons/:id", () => {
+    it("should return the Pokemon with the given id.", async () => {
+      const res = await request.get("/api/pokemons/42");
+      res.should.be.json;
+      let expect;
+      for (const poke of pokemon) {
+        if (poke.id === "042") {
+          expect = poke;
+        }
+      }
+      JSON.parse(res.text).should.eql(expect);
+    });
+  });
+  describe("GET /api/pokemons/:name", () => {
+    it("It should return the Pokemon with given name.", async () => {
+      let expect = 0;
+      for (const poke of pokemon) {
+        if (poke.name === "Mew") {
+          expect = poke;
+        }
+      }
+      const res = await request.get("/api/pokemons/Mew");
+      res.should.be.json;
+      JSON.parse(res.text).should.eql(expect);
+    });
+  });
+  describe("PATCH /api/pokemons/:idOrName", () => {
+    it("should make partial modifications to a Pokemon", async () => {
+      const res = await request
+        .patch("/api/pokemons/Mew")
+        .send({ name: "Meeeeeew" });
+      let expect = 0;
+      for (const poke of pokemon) {
+        if (poke.name === "Mew") {
+          expect = poke;
+        }
+      }
+    });
   });
 });
